c1ab48e0511550f8c26203cab435988869dc70f6
Рамиль Сагидуллин <ramilsagidullin@Air-Ramil.Dlink>
2020-07-29 15:20:52 +0300

    Small implementation of logic

--- calc.js
+++ calc.js
@@ -1,5 +1,103 @@
 "use strict";
-function type(text) {
-    return text;
+var Calculator = (function () {
+    function Calculator() {
+        var _a;
+        this.operations = (_a = {},
+            _a["+"] = function (a, b) {
+                return String(a + b);
+            },
+            _a["-"] = function (a, b) {
+                return String(a - b);
+            },
+            _a["×"] = function (a, b) {
+                return String(a * b);
+            },
+            _a["÷"] = function (a, b) {
+                return String(a / b);
+            },
+            _a);
+        this.sum = 0;
+    }
+    return Calculator;
+}());
+function addEvents() {
+    var field = document.querySelector(".field"), AC = document.querySelector("#AC"), numbers = document.querySelectorAll(".number, .doubled, #comma"), signs = document.querySelectorAll(".sign"), equiv = document.querySelector("#equiv");
+    var calc = new Calculator();
+    var repeated = false;
+    var toggler = false;
+    var calced = false;
+    var access = false;
+    var storage;
+    var a, b;
+    var func;
+    field.innerHTML = "0";
+    AC.addEventListener("click", function () {
+        calc.sum = 0;
+        field.innerHTML = String(calc.sum);
+        toggler = false;
+        calced = false;
+        repeated = false;
+        access = false;
+    });
+    var _loop_1 = function (i) {
+        numbers[i].addEventListener("click", function () {
+            if ((field.innerHTML == "0" || toggler || calced) && !access) {
+                field.innerHTML = numbers[i].innerHTML;
+                if (calced) {
+                    toggler = false;
+                }
+                if (toggler) {
+                    access = true;
+                }
+            }
+            else {
+                field.innerHTML += numbers[i].innerHTML;
+            }
+            repeated = false;
+        });
+    };
+    for (var i = 0; i < numbers.length; i++) {
+        _loop_1(i);
+    }
+    var _loop_2 = function (i) {
+        signs[i].addEventListener("click", function () {
+            var tempSign = signs[i].innerHTML;
+            if (!toggler || tempSign != storage) {
+                a = Number(field.innerHTML);
+                toggler = true;
+                storage = tempSign;
+                func = calc.operations[signs[i].innerHTML];
+            }
+            else {
+                b = Number(field.innerHTML);
+                if (!calced) {
+                    calced = true;
+                    field.innerHTML = calc.operations[signs[i].innerHTML](a, b);
+                }
+            }
+            access = false;
+            repeated = false;
+        });
+    };
+    for (var i = 0; i < signs.length; i++) {
+        _loop_2(i);
+    }
+    equiv.addEventListener("click", function () {
+        if (toggler) {
+            if (!repeated) {
+                b = Number(field.innerHTML);
+                if (calced) {
+                    a = b;
+                }
+                field.innerHTML = func(a, b);
+                repeated = true;
+            }
+            else {
+                a = Number(field.innerHTML);
+                field.innerHTML = func(a, b);
+            }
+            access = false;
+        }
+    });
 }
-alert(type(23));
+addEvents();
--- calc.ts
+++ calc.ts
@@ -1,7 +1,133 @@
 "use strict";
 
-function type(text: string) {
-    return text;
+interface Calc {
+    operations: object;
+    sum: number;
 }
 
-alert(type("ROFL"));
+class Calculator implements Calc {
+    operations: object;
+    sum: number;
+
+    constructor() {
+        this.operations = {
+            ["+"](a: number, b: number): string {
+                return String(a + b);
+            },
+            ["-"](a: number, b: number): string {
+                return String(a - b);
+            },
+            ["×"](a: number, b: number): string {
+                return String(a * b);
+            },
+            ["÷"](a: number, b: number): string {
+                return String(a / b);
+            }
+        };
+
+        this.sum = 0;
+    }
+}
+
+// Event listeners for all the buttons
+function addEvents() {
+    const field   = document.querySelector(".field"),
+          AC      = document.querySelector("#AC"),
+          numbers = document.querySelectorAll(".number, .doubled, #comma"),
+          signs   = document.querySelectorAll(".sign"),
+          equiv   = document.querySelector("#equiv");
+
+    const calc: Calculator = new Calculator();
+
+    let repeated: boolean = false;
+    let toggler: boolean = false;
+    let calced: boolean = false;
+    let access: boolean = false;
+    let storage: string;
+    let a, b: number;
+    let func;
+
+    // Initial zeroing of the field
+    field.innerHTML = "0";
+
+    // Event for button 'AC' - Zeroing
+    AC.addEventListener("click", function() {
+        calc.sum = 0;
+        field.innerHTML = String(calc.sum);
+        
+        toggler = false;
+        calced = false;
+        repeated = false;
+        access = false;
+    });
+
+    // Events for 'Number' buttons
+    for (let i = 0; i < numbers.length; i++) {
+        numbers[i].addEventListener("click", function() {
+            if ((field.innerHTML == "0" || toggler || calced) && !access) {
+                field.innerHTML = numbers[i].innerHTML;
+                
+                if (calced) {
+                    toggler = false;
+                }
+
+                if (toggler) {
+                    access = true;
+                }
+            }
+            else {
+                field.innerHTML += numbers[i].innerHTML;
+            }
+
+            repeated = false;
+        });
+    }
+
+    // Events for 'Sign' buttons
+    for (let i = 0; i < signs.length; i++) {
+        signs[i].addEventListener("click", function() {
+            let tempSign = signs[i].innerHTML;
+
+            if (!toggler || tempSign != storage) {
+                a = Number(field.innerHTML);
+                toggler = true;
+                storage = tempSign;
+                func = calc.operations[signs[i].innerHTML];
+            }
+            else {
+                b = Number(field.innerHTML);
+
+                if (!calced) {
+                    calced = true;
+                    field.innerHTML = calc.operations[signs[i].innerHTML](a, b);
+                }
+            }
+            
+            access = false;
+            repeated = false;
+        });
+    }
+
+    equiv.addEventListener("click", function() {
+        if (toggler) {
+            if (!repeated) {
+                b = Number(field.innerHTML);
+                
+                if (calced) {
+                    a = b;
+                }
+
+                field.innerHTML = func(a, b);
+                repeated = true;
+            }
+            else {
+                a = Number(field.innerHTML);
+                field.innerHTML = func(a, b);
+            }
+
+            access = false;
+        }
+    });
+}
+
+addEvents();
